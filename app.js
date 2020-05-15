3d7278b77450c67a132445a498859c446b7fb853
Denny <denny1011133@gmail.com>
2020-05-15 23:09:27 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const Restaurant = require('./models/restaurant.js')
+const bodyParser = require('body-parser')
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 // 取得資料庫連線狀態
@@ -20,13 +21,28 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 //瀏覽所有餐廳清單
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
 })
+
+//導向新增頁面
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+//新增一筆餐廳資訊
+app.post('/restaurants', (req, res) => {
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
+  console.log(req.body)
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant: restaurant })
