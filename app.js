48926db8d9292aee0ac0ab2b735eb80f8006ab14
Denny <denny1011133@gmail.com>
2020-05-15 23:20:43 +0800

    feat: add show page

--- app.js
+++ app.js
@@ -37,15 +37,17 @@ app.get('/restaurants/new', (req, res) => {
 //新增一筆餐廳資訊
 app.post('/restaurants', (req, res) => {
   const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
-  console.log(req.body)
   return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
+  const id = req.params.restaurant_id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
 
 })
 app.get('/search', (req, res) => {
