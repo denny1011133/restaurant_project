e13996169ed7c8eb270752404368abdc7d9b02d4
Denny <denny1011133@gmail.com>
2020-06-07 09:44:35 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const session = require('express-session')
+const usePassport = require('./config/passport')
 const app = express()
 const PORT = process.env.PORT || 3000
 const exphbs = require('express-handlebars')
@@ -13,14 +14,17 @@ require('./config/mongoose')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
   saveUninitialized: true
 }))
+
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
 app.use(routes)
 
 
