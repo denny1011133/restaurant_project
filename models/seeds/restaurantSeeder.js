792298689795e00fea66bad7e4cac896cd756f32
Denny <denny1011133@gmail.com>
2020-06-07 20:54:58 +0800

    add seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,19 +1,54 @@
 const Restaurant = require('../restaurant.js')
-const restaurantList = require('/restaurant.json')
+const User = require('../user')
+const restaurantList = require('../restaurant.json')
 const db = require('../../config/mongoose')
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const SEED_USER = [{
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678'
+},
+{
+  name: 'user2',
+  email: 'user2@example.com',
+  password: '12345678'
+}]
 db.once('open', () => {
-  restaurantList.results.forEach(i => {
-    Restaurant.create({
-      "name": i.name,
-      "name_en": i.name_en,
-      "category": i.category,
-      "image": i.image,
-      "location": i.location,
-      "phone": i.phone,
-      "google_map": i.google_map,
-      "rating": i.rating,
-      "description": i.description
-    })
+  SEED_USER.forEach(seed => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(seed.password, salt))
+      .then(hash => User.create({
+        name: seed.name,
+        email: seed.email,
+        password: hash
+      }))
+      .then((user) => {
+        const userId = user._id
+        Promise.all(Array.from(
+          { length: 3 },
+          (_, i) => {
+            for (let i = 0; i < 3; i++) {
+              Restaurant.create({
+                "name": restaurantList.results[i].name,
+                "name_en": restaurantList.results[i].name_en,
+                "category": restaurantList.results[i].category,
+                "image": restaurantList.results[i].image,
+                "location": restaurantList.results[i].location,
+                "phone": restaurantList.results[i].phone,
+                "google_map": restaurantList.results[i].google_map,
+                "rating": restaurantList.results[i].rating,
+                "description": restaurantList.results[i].description,
+                "_id": userId,
+              })
+            }
+          }
+        ))
+      })
   })
   console.log('mongodb connected!')
 })
