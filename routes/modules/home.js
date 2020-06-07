05930a990bd82c24c98061aea485efe1165fa7b2
Denny <denny1011133@gmail.com>
2020-06-07 13:01:19 +0800

    feat: add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,7 +4,8 @@ const Restaurant = require('../../models/restaurant')
 
 //瀏覽所有餐廳清單
 router.get('/', (req, res) => {
-  Restaurant.find()
+  const userId = req.user._id
+  Restaurant.find({ userId })
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
 })
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -8,33 +8,35 @@ router.get('/new', (req, res) => {
 
 //新增一筆餐廳資訊
 router.post('/', (req, res) => {
+  const userId = req.user._id
   const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
-  console.log(req.body)
-  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description, userId })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 //顯示單筆餐廳資訊
 router.get('/:restaurant_id', (req, res) => {
-  const id = req.params.restaurant_id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.restaurant_id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then((restaurant) => res.render('show', { restaurant }))
     .catch(error => console.log(error))
 })
 //導向編輯單筆餐廳畫面
 router.get('/:restaurant_id/edit', (req, res) => {
-  const id = req.params.restaurant_id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.restaurant_id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then((restaurant) => res.render('edit', { restaurant }))
     .catch(error => console.log(error))
 })
 //編輯單筆餐廳資訊
 router.put('/:restaurant_id', (req, res) => {
-  const id = req.params.restaurant_id
-  // const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.restaurant_id
+  return Restaurant.findOne({ _id, userId })
     .then(restaurant => {
       restaurant = Object.assign(restaurant, req.body)
       return restaurant.save()
@@ -44,8 +46,9 @@ router.put('/:restaurant_id', (req, res) => {
 })
 //刪除特定單筆餐廳資訊
 router.delete('/:restaurant_id', (req, res) => {
-  const id = req.params.restaurant_id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.restaurant_id
+  return Restaurant.findOne({ _id, userId })
     .then(restaurant => restaurant.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
