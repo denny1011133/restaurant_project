9765a2469f7e726c412590ba95e5a7c886753569
Denny <denny1011133@gmail.com>
2020-06-07 21:38:11 +0800

    fix seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -18,39 +18,62 @@ const SEED_USER = [{
   password: '12345678'
 }]
 db.once('open', () => {
-  SEED_USER.forEach(seed => {
-    bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(seed.password, salt))
-      .then(hash => User.create({
-        name: seed.name,
-        email: seed.email,
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER[0].password, salt))
+    .then((hash) => {
+      return User.create({
+        name: SEED_USER[0].name,
+        email: SEED_USER[0].email,
         password: hash
-      }))
-      .then((user) => {
-        const userId = user._id
-        Promise.all(Array.from(
-          { length: 3 },
-          (_, i) => {
-            Restaurant.create({
-              "name": restaurantList.results[i].name,
-              "name_en": restaurantList.results[i].name_en,
-              "category": restaurantList.results[i].category,
-              "image": restaurantList.results[i].image,
-              "location": restaurantList.results[i].location,
-              "phone": restaurantList.results[i].phone,
-              "google_map": restaurantList.results[i].google_map,
-              "rating": restaurantList.results[i].rating,
-              "description": restaurantList.results[i].description,
-              "userId": userId,
-            })
-          }
-        ))
-      })
-      .then(() => {
-        console.log('done.')
-        process.exit()
-      })
-  })
+      }).catch((err) => console.log(err))
+    })
+    .then((user) => {
+      const userId = user._id
+      Promise.all(Array.from(
+        { length: 3 },
+        (_, i) => Restaurant.create({
+          name: restaurantList.results[i].name,
+          name_en: restaurantList.results[i].name_en,
+          category: restaurantList.results[i].category,
+          image: restaurantList.results[i].image,
+          location: restaurantList.results[i].location,
+          phone: restaurantList.results[i].phone,
+          google_map: restaurantList.results[i].google_map,
+          rating: restaurantList.results[i].rating,
+          description: restaurantList.results[i].description,
+          userId: userId,
+        })
+      ))
+    })
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER[1].password, salt))
+    .then((hash) => {
+      return User.create({
+        name: SEED_USER[1].name,
+        email: SEED_USER[1].email,
+        password: hash
+      }).catch((err) => console.log(err))
+    })
+    .then((user) => {
+      const userId = user._id
+      Promise.all(Array.from(
+        { length: 3 },
+        (_, i) => Restaurant.create({
+          name: restaurantList.results[i].name,
+          name_en: restaurantList.results[i].name_en,
+          category: restaurantList.results[i].category,
+          image: restaurantList.results[i].image,
+          location: restaurantList.results[i].location,
+          phone: restaurantList.results[i].phone,
+          google_map: restaurantList.results[i].google_map,
+          rating: restaurantList.results[i].rating,
+          description: restaurantList.results[i].description,
+          userId: userId,
+        })
+      ))
+    })
+
   console.log('mongodb connected!')
 })
